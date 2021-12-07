616c329318eff6ad960c96cce2212c7018d0a90e
Travis Blevins <travismblevins@gmail.com>
2021-12-07 13:48:35 -0500

    Update Expense.py

--- budget/Expense.py
+++ budget/Expense.py
@@ -1,15 +1,18 @@
 import csv
 from datetime import datetime
+
 class Expense():
     def __init__(self, date_str, vendor, category, amount):
         self.date_time = datetime.strptime(date_str, '%m/%d/%Y %H:%M:%S')
         self.vendor = vendor
         self.category = category
         self.amount = amount
+
 class Expenses():
     def __init__(self):
         self.list = []
         self.sum = 0
+
     # Read in the December spending data, row[2] is the $$, and need to format $$
     def read_expenses(self,filename):
         with open(filename, newline='') as csvfile:
@@ -48,5 +51,19 @@ class Expenses():
                     food_expenses2.add(i)
                 else:
                     unnecessary_expenses2.add(i)
-            
+
+            return [necessary_expenses2, food_expenses2, unnecessary_expenses2] 
             return [necessary_expenses2, food_expenses2, unnecessary_expenses2]
+
+    def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list 
+                            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
+                                x.category == 'Classes'  or x.category == 'Utilities' or 
+                                x.category == 'Mortgage'}
+
+        food_expenses = {x for x in self.list 
+                            if x.category == 'Groceries' or x.category == 'Eating Out'}
+
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
