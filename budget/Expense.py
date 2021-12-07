683b7214b3c8c000188349cbcd9bddc1a2da0b77
Travis Blevins <travismblevins@gmail.com>
2021-12-07 13:01:43 -0500

    Update Expense.py

--- budget/Expense.py
+++ budget/Expense.py
@@ -1,19 +1,15 @@
 import csv
 from datetime import datetime
-
 class Expense():
     def __init__(self, date_str, vendor, category, amount):
         self.date_time = datetime.strptime(date_str, '%m/%d/%Y %H:%M:%S')
         self.vendor = vendor
         self.category = category
         self.amount = amount
-
-
 class Expenses():
     def __init__(self):
         self.list = []
         self.sum = 0
-
     # Read in the December spending data, row[2] is the $$, and need to format $$
     def read_expenses(self,filename):
         with open(filename, newline='') as csvfile:
@@ -25,18 +21,32 @@ class Expenses():
                 self.list.append(Expense(row[0],row[1], row[2], amount))
                 self.sum += amount
 
+    def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list 
+                            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
+                                x.category == 'Classes'  or x.category == 'Utilities' or 
+                                x.category == 'Mortgage'}
+
+        food_expenses = {x for x in self.list 
+                            if x.category == 'Groceries' or x.category == 'Eating Out'}
+
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
+
+
     def categorize_for_loop(self):
-            necessary_expenses = set()
-            food_expenses = set()
-            unnecessary_expenses = set()
+            necessary_expenses2 = set()
+            food_expenses2 = set()
+            unnecessary_expenses2 = set()
             for i in self.list:
-                if (i.category == 'Phone'    or i.category == 'Auto and Gas' or 
+                if (i.category == 'Phone'      or i.category == 'Auto and Gas' or 
                     i.category == 'Classes'  or i.category == 'Utilities' or 
                     i.category == 'Mortgage'): 
-                    necessary_expenses.add(i)
+                    necessary_expenses2.add(i)
                 elif(i.category == 'Groceries' or i.category == 'Eating Out'):
-                    food_expenses.add(i)
+                    food_expenses2.add(i)
                 else:
-                    unnecessary_expenses.add(i)
+                    unnecessary_expenses2.add(i)
             
-            return [necessary_expenses, food_expenses, unnecessary_expenses]
+            return [necessary_expenses2, food_expenses2, unnecessary_expenses2]
